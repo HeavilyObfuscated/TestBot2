ab2d388a49ff9cd5f87a25c293b4407ce181872f
HeavilyObfuscated <charliecrooks98@gmail.com>
2017-07-19 12:04:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Shut up David"();
 
   options = {
     hostname: 'api.groupme.com',
